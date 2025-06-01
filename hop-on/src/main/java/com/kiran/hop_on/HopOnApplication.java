a52d664ccf4423ad94bed27558eae92ba90258c3
Kiran <88050617+Kiran9223@users.noreply.github.com>
2025-05-31 17:34:47 -0700

    project init

---
+++ hop-on/src/main/java/com/kiran/hop_on/HopOnApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.hop_on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopOnApplication.class, args);
+	}
+
+}
